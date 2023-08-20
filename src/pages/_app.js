43da9fbd9c212fa41ad8d6b683c015c9b727a58b
Mattyx <mattyx22@gmail.com>
2023-08-20 11:13:08 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,21 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "@/styles/globals.css";
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
+    <>
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-HW8DKGYFJE" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-HW8DKGYFJE');
+        `}
+      </Script>
     <Component {...pageProps} />
+    </>
   );
 }
