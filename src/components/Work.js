d319596c63894fca243f6359908a15e7d5ee7245
Mattyx <mattyx22@gmail.com>
2024-10-10 10:14:00 +0200

    Added descriptions

--- src/components/Work.js
+++ src/components/Work.js
@@ -47,27 +47,113 @@ const Work = () => {
 
             <Row>
                 <div className={styles.workContainerRight}>
-                    <video src="/work/targmed-refundacja.mp4" loop autoPlay muted className={styles.video}></video>
+                    <div className={styles.videoContainer}>
+                        <video src="/work/targmed-refundacja.mp4" loop autoPlay muted className={styles.video}></video>
+                        <div className={styles.overlay}>
+                            <div className={styles.firstLine}>Targmed-refundacja.pl</div>
+                            <div className={styles.secondLine}>
+                                <p>Creating an online store that fulfills orders with NFZ (Polish National Health Fund) subsidies.</p>
+                                <p>A user with a reimbursement order from a doctor, provides the PESEL number and order pin. The store connects to the NHF API to verify the order and determine the health fund's surcharge.</p>
+                                <p>Once verified correctly, the user receives lower prices at which to place the order.</p>
+                                <hr className="hr" />
+                                <table className={styles.stack}>
+                                    <tr>
+                                        <th>E-Commerce:</th>
+                                        <th>API:</th>
+                                    </tr>
+                                    <tr>
+                                        <td>- Wordpress / Woocommerce</td>
+                                        <td>- Python</td>
+                                    </tr>
+                                    <tr>
+                                        <td>- PHP</td>
+                                        <td>- Node.js</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div className={styles.workContainerLeft}>
-                    <video src="/work/cw.mp4" loop autoPlay muted className={styles.video}></video>
+                    <div className={styles.videoContainer}>
+                        <video src="/work/cw.mp4" loop autoPlay muted className={styles.video}></video>
+                        <div className={styles.overlay}>
+                            <div className={styles.firstLine}>CreatorsWorld</div>
+                            <div className={styles.secondLine}>
+                                <p>A self-developed marketplace system to handle sales and purchases of user-created digital products.</p>
+                                <hr className="hr" />
+                                <table className={styles.stack}>
+                                    <tr>
+                                        <td>- React</td>
+                                    </tr>
+                                    <tr>
+                                        <td>- Node.js</td>
+                                    </tr>
+                                    <tr>
+                                        <td>- MongoDB</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div className={styles.workContainerRight}>
-                    <video src="/work/kaldera.mp4" loop autoPlay muted className={styles.video}></video>
+                    <div className={styles.videoContainer}>
+                        <video src="/work/kaldera.mp4" loop autoPlay muted className={styles.video}></video>
+                        <div className={styles.overlay}>
+                            <div className={styles.firstLine}>Kaldera.com.pl</div>
+                            <div className={styles.secondLine}>
+                                <p>The site of a company that sells heating equipment and home plumbing.</p>
+                                <hr className="hr" />
+                                <p>- Wordpress</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div className={styles.workContainerLeft}>
-                    <video src="/work/ra.mp4" loop autoPlay muted className={styles.video}></video>
+                    <div className={styles.videoContainer}>
+                        <video src="/work/ra.mp4" loop autoPlay muted className={styles.video}></video>
+                        <div className={styles.overlay}>
+                            <div className={styles.firstLine}>Rearaxle.co.uk</div>
+                            <div className={styles.secondLine}>
+                                <p>Web store for a car parts selling company.</p>
+                                <hr className="hr" />
+                                <p>- Wordpress / Woocommerce</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div className={styles.workContainerRight}>
-                    <video src="/work/bosch.mp4" loop autoPlay muted className={styles.video}></video>
+                    <div className={styles.videoContainer}>
+                        <video src="/work/bosch.mp4" loop autoPlay muted className={styles.video}></video>
+                        <div className={styles.overlay}>
+                            <div className={styles.firstLine}>Bosch.swidnica.pl</div>
+                            <div className={styles.secondLine}>
+                                <p>Website for a car service.</p>
+                                <hr className="hr" />
+                                <p>- Wordpress</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div className={styles.workContainerLeft}>
-                    <video src="/work/loca.mp4" loop autoPlay muted className={styles.video}></video>
+                    <div className={styles.videoContainer}>
+                        <video src="/work/loca.mp4" loop autoPlay muted className={styles.video}></video>
+                        <div className={styles.overlay}>
+                            <div className={styles.firstLine}>Loca</div>
+                            <div className={styles.secondLine}>
+                                <p>UI / UX design for a website and mobile app that helps owners of businesses to track their drivers.</p>
+                                <hr className="hr" />
+                                <p>- Figma</p>
+                                <p>- Photoshop</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </Row>
 
